58bf525762af1240cbbf64b503b2457da0b9ca35
Phoebe Wang <phoebewang@Phoebede-MacBook.local>
2020-11-11 18:34:32 +0900

    add read file products.py

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+#Read file
 products = []
+with open ('products.txt', 'r', encoding='utf-8') as f:
+    for line in f:
+        if ('item,price') in line:
+            continue #skip to the next loop
+        name, price = line.strip().split(',')
+        products. append ([name, price])
+print (products)
+
+# User input
 while True: 
 	name = input ('Please enter the product name:')
 	if name == 'q':
@@ -7,9 +17,11 @@ while True:
 	products.append([name,price])
 print(products)
 
+# Print all purchasing record
 for p in products:
 	print('The price of', p[0], 'is', p[1])
 
+# Write in file
 with open ('products.csv', 'w', encoding= 'utf-8') as f:
     f.write('Item, Price\n')
     for p in products:
