ab4d38118f4c0c7e37c3151529848c51f0943ea5
Phoebe Wang <phoebewang@Phoebede-MacBook.local>
2020-11-12 17:30:43 +0900

    add file exist products.py

--- products.py
+++ products.py
@@ -1,12 +1,22 @@
-#Read file
+import os # operating system
+
 products = []
-with open ('products.txt', 'r', encoding='utf-8') as f:
-    for line in f:
-        if ('item,price') in line:
-            continue #skip to the next loop
-        name, price = line.strip().split(',')
-        products. append ([name, price])
-print (products)
+if os.path.isfile('products.csv'):
+    print('yeah! Found the file!')
+    with open ('products.csv', 'r', encoding='utf-8') as f:
+        for line in f:
+            if ('name,price') in line:
+                continue #skip to the next loop
+            name, price = line.strip().split(',')
+            products. append ([name, price])
+    print (products)
+
+else:
+    print('File not found.')
+
+
+#Read file
+
 
 # User input
 while True: 
@@ -23,6 +33,6 @@ for p in products:
 
 # Write in file
 with open ('products.csv', 'w', encoding= 'utf-8') as f:
-    f.write('Item, Price\n')
+    f.write('Name, Price\n')
     for p in products:
         f.write(p[0] + ',' + str(p[1]) + '\n')
